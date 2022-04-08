3aea0ad94e3cb2a1c064c6645754518c4c7aee18
Juandi <joa102@inlumine.ual.es>
2022-04-08 21:58:45 +0200

    sección 6 tutorial modificando api para springboot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -10,13 +11,24 @@ import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SuperpowerFormComponent } from './superpower-form/superpower-form.component';
+// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+// import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    // HttpClientInMemoryWebApiModule.forRoot(
+    //   InMemoryDataService, { dataEncapsulation: false }
+    // )
   ],
   declarations: [
     AppComponent,
@@ -24,7 +36,8 @@ import { SuperpowerFormComponent } from './superpower-form/superpower-form.compo
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    SuperpowerFormComponent
+    SuperpowerFormComponent,
+    HeroSearchComponent
   ],
   bootstrap: [ AppComponent ]
 })
